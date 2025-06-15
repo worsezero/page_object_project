a7b25173e28d23e192aeee45a5eef441638af576
worsezero <vladimirkalugin0@gmail.com>
2025-06-15 05:14:25 +0300

    Update test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -85,7 +85,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
-
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
     page = ProductPage(browser, link)
